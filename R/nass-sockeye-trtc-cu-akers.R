b516e804abe4a78bd825c56ca5c970319d120093
andy <andy@coastlandresearch.com>
2025-06-05 22:20:56 -0700

    updated skeena and nass to functions and source files and it works!

--- R/nass-sockeye-trtc-cu-akers.R
+++ R/nass-sockeye-trtc-cu-akers.R
@@ -1,24 +1,26 @@
-
-library(data.table)
-library(tidyverse)
-
-data<-fread("data-raw/sockeye-nass-trtc data.csv")
-
-data$`AK ER`<-data$`Total ER`-data$`CDN ER`
+#plot functions for nass trtc line and boxplots
 
 #line plot of ak ers for each nass cu
-ggplot(data,aes(x=Year,y=`AK ER`))+
+make.nass.trtc.lineplot<-function(data) {
+  
+  data$`AK ER`<-data$`Total ER`-data$`CDN ER`
+  
+  ggplot(data,aes(x=Year,y=`AK ER`))+
   geom_point()+geom_line()+
   facet_wrap(CU_Name~.,ncol=3)+
   labs(y="Alaskan Exploitation Rate")+
   theme_bw()
+}
 
 #boxplot of ak ers for each nass cu
-ggplot(data,aes(x=`AK ER`,y=reorder(CU_Name,`AK ER`,median),fill=CU_Name))+
+make.nass.trtc.boxplot<-function(data) {
+  
+  data$`AK ER`<-data$`Total ER`-data$`CDN ER`
+  
+  ggplot(data,aes(x=`AK ER`,y=reorder(CU_Name,`AK ER`,median),fill=CU_Name))+
   geom_boxplot(outlier.shape = NA) +
   geom_jitter(height=.1)+
   labs(y="Conservation Unit",x="Alaskan Exploitation Rate")+
   theme_bw()+
   guides(fill="none")
-
-#proportion cdn vs us catch
+}
--- R/skeena-sockeye-trtc-cu-akers.R
+++ R/skeena-sockeye-trtc-cu-akers.R
@@ -1,24 +1,26 @@
+#plot functions for skeena trtc line and boxplots
 
-library(data.table)
-library(tidyverse)
-library(ggridges)
+#line plot of ak ers for each nass cu
+make.skeena.trtc.lineplot<-function(data) {
+  
+  data$`AK ER`<-data$`Total ER`-data$`CDN ER`
+  
+  ggplot(data,aes(x=Year,y=`AK ER`))+
+    geom_point()+geom_line()+
+    facet_wrap(CU_Name~.,ncol=3)+
+    labs(y="Alaskan Exploitation Rate")+
+    theme_bw()
+}
 
-data<-fread("data-raw/sockeye-skeena-trtc data.csv")
-
-data$`AK ER`<-data$`Total ER`-data$`CDN ER`
-
-#line plot of ak ers for each skeena cu
-ggplot(data,aes(x=Year,y=`AK ER`))+
-  geom_point()+geom_line()+
-  facet_wrap(CU_Name~.,ncol=3)+
-  labs(y="Alaskan Exploitation Rate")+
-  theme_bw()
-
-#boxplot of ak ers for each skeena cu
-ggplot(data,aes(x=`AK ER`,y=reorder(CU_Name,`AK ER`,median),fill=CU_Name))+
-  geom_boxplot(outlier.shape = NA)+
-  geom_jitter(height=.1)+
-  labs(y="Conservation Unit",x="Alaskan Exploitation Rate")+
-  theme_bw()+
-  guides(fill="none")
-
+#boxplot of ak ers for each nass cu
+make.skeena.trtc.boxplot<-function(data) {
+  
+  data$`AK ER`<-data$`Total ER`-data$`CDN ER`
+  
+  ggplot(data,aes(x=`AK ER`,y=reorder(CU_Name,`AK ER`,median),fill=CU_Name))+
+    geom_boxplot(outlier.shape = NA) +
+    geom_jitter(height=.1)+
+    labs(y="Conservation Unit",x="Alaskan Exploitation Rate")+
+    theme_bw()+
+    guides(fill="none")
+}
