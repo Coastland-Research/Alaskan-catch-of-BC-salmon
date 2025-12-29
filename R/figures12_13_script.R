0db7479c5930a3bb3d0d880dcfab36b8911720de
elizabeth-hirsch <ehirsch@unbc.ca>
2025-12-28 21:58:55 -0800

    start figure 12-13 script

--- R/figures12_13_script.R
+++ R/figures12_13_script.R
@@ -1,9 +1,28 @@
 # Script for Figures 12 and 13
 # figure 12: weekly catch of Pink salmon in district 104 seine fishery 
 # figure 13: SEAK pink and sockeye catch in district 104 sub-areas by statistical week
+
+library(data.table)
+library(tidyverse)
+library(RColorBrewer)
+library(ggpubr)
+library(dplyr)
+library(tidyr)
+
 harvests<-fread("data-raw/SEAK D101-D106 harvest by district by week.csv")
-view(harvests)
-names(harvests)
+
 harvests %>%
-  ggplot(aes(x = `Stat Week`, y = `Number of Fish (estimated)`, fill = `District Number`)) +
+  mutate(district = as.factor(`District Number`)) %>%
+  mutate(`Stat Week` = as.factor(`Stat Week`)) %>%
+  filter(`Number Of Fish (estimated)` != "Confidential") %>%
+  mutate(`Number Of Fish (estimated)` = as.numeric(`Number Of Fish (estimated)`)) %>%
+  ggplot(aes(x = `Stat Week`, y = `Number Of Fish (estimated)`)) +
+  geom_boxplot() +
+  facet_wrap(~`District Number`)
+
+# need to figure out what's wrong with the data grouping - creates scatterplot instead
+# of boxplots
+
+
+
   
