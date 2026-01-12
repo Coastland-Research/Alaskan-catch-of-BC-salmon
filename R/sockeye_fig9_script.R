f505cd5e9e89287cc2809e90e529de8454d87a6b
elizabeth-hirsch <ehirsch@unbc.ca>
2026-01-12 15:14:08 -0800

    break apart us vs canadian catch into separate Skeena and Nass plots

--- R/sockeye_fig9_script.R
+++ R/sockeye_fig9_script.R
@@ -11,18 +11,37 @@ totsn$Total.Catch=totsn$US.Catch+totsn$CDN.Catch
 totsn$USp=totsn$US.Catch/totsn$Total.Catch
 totsn$CDNp=totsn$CDN.Catch/totsn$Total.Catch
 
-tot.catch<-totsn%>%select(Area,Year,US.Catch,CDN.Catch,Total.Catch)%>%
-  pivot_longer(3:5,names_to="Fishery",values_to="Catch")
+tot.catch<-totsn%>%select(Area,Year,US.Catch,CDN.Catch)%>%
+  pivot_longer(3:4,names_to="Fishery",values_to="Catch")
 
-ggplot(tot.catch,aes(x=Year,y=Catch/1000,color=Fishery))+
+#Skeena region
+tot.catch.skeena <- tot.catch %>%
+  filter(Area == "Skeena")
+  
+sk_us_cdn <- ggplot(tot.catch.skeena,aes(x=Year,y=Catch/1000,color=Fishery))+
   geom_point(alpha=.5)+geom_line(alpha=.5)+
   theme_bw()+
-  scale_colour_manual(values=c("red","black","blue"))+
-  facet_wrap(~Area,ncol=1,scales="free_y")+
+  scale_colour_manual(values=c("red","blue"))+
+  # facet_wrap(~Area,ncol=1,scales="free_y")+
   labs(x="Year",y="Catch (Thousands)",
-       title=paste0("US and CDN Total Catch of Skeena and Nass sockeye"))+
+       title=paste0("US and CDN Total Catch of Skeena sockeye"))+
   theme(legend.position ="right")
 
-# ggsave("SX SEAK vs CDN Catch.png",dpi=600,height=6,width=6)
+sk_us_cdn
+
+#Nass region
+tot.catch.nass <- tot.catch %>%
+  filter(Area == "Nass")
+
+nass_us_cdn <- ggplot(tot.catch.nass,aes(x=Year,y=Catch/1000,color=Fishery))+
+  geom_point(alpha=.5)+geom_line(alpha=.5)+
+  theme_bw()+
+  scale_colour_manual(values=c("red","blue"))+
+  # facet_wrap(~Area,ncol=1,scales="free_y")+
+  labs(x="Year",y="Catch (Thousands)",
+       title=paste0("US and CDN Total Catch of Nass sockeye"))+
+  theme(legend.position ="right")
+
+nass_us_cdn
 
 
