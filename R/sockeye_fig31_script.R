c926ac4bc9dd30f692f351536c1a7d0ce1c48991
elizabeth-hirsch <ehirsch@unbc.ca>
2026-01-13 21:57:43 -0800

    figure 31 clean up code

--- R/sockeye_fig31_script.R
+++ R/sockeye_fig31_script.R
@@ -1,14 +1,9 @@
-
+# figure 31 - sockeye stock composition by week for Districts 101 and 104 (2016-2018)
 
 #### SOCKEYE D101 and D014 WEEKLY STOCK COMPS ####
 data<-read.csv("data-raw/2022-01-10d101.104GSI2006_2018.csv")
-names(data)
-
 
 scold<-data%>%filter(year>2015)
-
-nb.cols <- 14
-# mycolors <- colorRampPalette(brewer.pal(8, "Set1"))(nb.cols)
 
 ggplot(scold,aes(x=week,y=mean/100,fill=stock))+
   geom_bar(stat="identity")+
