412752f1addb2dcb01a9f386087a9c53c0427ad2
elizabeth-hirsch <ehirsch@unbc.ca>
2026-01-20 13:38:18 -0800

    split Chinook figure 24 into Skeena, Central Coast, and Nass regions

--- R/chinook_er_cu.R
+++ R/chinook_er_cu.R
@@ -6,13 +6,40 @@ make.cn.er.cu <- function(seak_er){
   peo<-seak_er%>%filter(SpeciesId%in%c("CN"))%>%filter(!CU_Name%in%c("Haida Gwaii-North",
                                                                 "NORTH & CENTRAL COAST-LATE TIMING",
                                                                 "NORTH & CENTRAL COAST-EARLY TIMING",
-                                                                "Dean River"))
+                                                                "Dean River")) %>%
+    mutate(Region = factor(case_when(CU_Name %in% c("Wannock", "Rivers Inlet", "Docee", "Bella Coola-Bentinck")~"Central Coast",
+                                     CU_Name %in% c("Upper Nass", "Portland Sound-Observatory Inlet-Lower Nass")~"Nass",
+                                     CU_Name %in% c("Ecstall", "Lower Skeena", "KALUM-EARLY TIMING", "KALUM-LATE TIMING",
+                                                    "Middle Skeena-large lakes", "Middle Skeena-mainstem tributaries",
+                                                    "Upper Bulkley River") ~ "Skeena")))
+  peo_sk <- peo %>%
+    filter(Region == "Skeena")
+  peo_cc <- peo %>%
+    filter(Region == "Central Coast")
+  peo_nass <- peo %>%
+    filter(Region == "Nass")
   
-  ggplot(peo,aes(x=Year,y=AKER))+
+  sk <- ggplot(peo_sk,aes(x=Year,y=AKER))+
   geom_point()+geom_line()+geom_smooth(alpha=.5,se=FALSE)+
   theme_bw()+
-  labs(y="SEAK Exploitation Rate",x="Year",color="",
-       title="SEAK Exploitation Rate by Conservation Unit",subtitle="Chinook Salmon (1985-2017)")+
+  labs(y = "", x = "", color="", title="Skeena")+
   theme(axis.text.y=element_text(size=7),legend.position = "bottom")+
-  facet_wrap(~CU_Name,ncol=3)
+    facet_wrap(~CU_Name)
+  cc <- ggplot(peo_cc,aes(x=Year,y=AKER))+
+    geom_point()+geom_line()+geom_smooth(alpha=.5,se=FALSE)+
+    theme_bw()+
+    labs(y = "", x = "", color="", title="Central Coast")+
+    theme(axis.text.y=element_text(size=7),legend.position = "bottom")+
+    facet_wrap(~CU_Name)
+  nass <- ggplot(peo_nass,aes(x=Year,y=AKER))+
+    geom_point()+geom_line()+geom_smooth(alpha=.5,se=FALSE)+
+    theme_bw()+
+    labs(y = "", x="Year",color="", title="Nass")+
+    theme(axis.text.y=element_text(size=7),legend.position = "bottom")+
+    facet_wrap(~CU_Name)
+  
+  grid.arrange(sk, cc, nass, heights = c(3.5, 3, 2), 
+               top = textGrob("SEAK Exploitation Rate on Chinook Salmon by Conservation Unit (1985-2017)"),
+               left = textGrob("SEAK Exploitation Rate", rot=90))
 } 
+
