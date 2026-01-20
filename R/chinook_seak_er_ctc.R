0a90eed76ecfe49437fa7312f46e345e684ef0cf
elizabeth-hirsch <ehirsch@unbc.ca>
2026-01-19 17:16:23 -0800

    filter out Transboundary region for Chinook figure 25

--- R/chinook_seak_er_ctc.R
+++ R/chinook_seak_er_ctc.R
@@ -9,18 +9,19 @@ make.cn.seak.ctc <- function(cn_new){
   
   cn.seak.toter<-cnstocks%>%select(Stock,Region,Catch.Year,SEAK.Troll,SEAK.Net,SEAK.Sport,
                                  Term.SEAK.Troll,Term.SEAK.Net,Term.SEAK.Sport)%>%
-  mutate(toter=rowSums(across("SEAK.Troll":"Term.SEAK.Sport")))
+  mutate(toter=rowSums(across("SEAK.Troll":"Term.SEAK.Sport"))) %>%
+    filter(Region != "Transboundary")
   
   cn.seak.all<-cnstocks%>%select(Stock,Region,Catch.Year,SEAK.Troll,SEAK.Net,SEAK.Sport,
                                Term.SEAK.Troll,Term.SEAK.Net,Term.SEAK.Sport)%>%
   pivot_longer("SEAK.Troll":"Term.SEAK.Sport",names_to="fishery",values_to="er")%>%
   filter(Region!="TBR")
   
-  c1<-cn.seak.toter%>%group_by(Stock,Region)%>%summarise(med=median(toter,na.rm=TRUE))
-  c2<-cn.seak.all%>%group_by(Stock,Region,fishery)%>%summarise(med=median(er,na.rm=TRUE))%>%
-  pivot_wider(.,names_from="fishery",values_from="med")
-  
-  c.data<-merge(c1,c2,by=c("Stock","Region"))
+  # c1<-cn.seak.toter%>%group_by(Stock,Region)%>%summarise(med=median(toter,na.rm=TRUE))
+  # c2<-cn.seak.all%>%group_by(Stock,Region,fishery)%>%summarise(med=median(er,na.rm=TRUE))%>%
+  # pivot_wider(.,names_from="fishery",values_from="med")
+  # 
+  # c.data<-merge(c1,c2,by=c("Stock","Region"))
   
   ggplot(cn.seak.toter,aes(x=Catch.Year,y=toter,color=Region))+
   geom_point()+geom_line()+
