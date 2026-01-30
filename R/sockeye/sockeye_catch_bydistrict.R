cc915a6181fa96bf5153a6dc44ac2879450f7b1c
elizabeth-hirsch <ehirsch@unbc.ca>
2026-01-30 09:43:08 -0800

    rename sockeye scripts, cleanup data read-in code

--- R/sockeye/sockeye_catch_bydistrict.R
+++ R/sockeye/sockeye_catch_bydistrict.R
@@ -1,7 +1,16 @@
 # SEAK sockeye catch figure 6 - total catches (all gears) by District by year
 
+make.catch.bydistrict <- function(sk_harv){
   
-make.catch.bydistrict <- function(sk_annual){
+  sk_annual <- sk_harv %>%
+    filter(`Species.Name` == "Sockeye Salmon") %>%
+    filter(`Number.Of.Fish..estimated.` != "Confidential") %>%
+    mutate(`Number.Of.Fish..estimated.` = as.numeric (`Number.Of.Fish..estimated.`)) %>%
+    filter(`District.Number` %in% 101:106) %>%
+    group_by(Year, `District.Number`) %>%
+    summarise(total_catch = sum(`Number.Of.Fish..estimated.`, na.rm = TRUE),
+              .groups = "drop")
+  
   ggplot(sk_annual,
        aes(x = Year, y = total_catch)) +
     geom_point(size = 1.5) +
